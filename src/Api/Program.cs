597b36b82beaa2e8ed83ab2d83e0ba7cb0c27d8e
GabrielTrigo <gabrieltrigo123@hotmail.com>
2025-12-06 13:47:44 -0300

    setup

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -16,6 +16,7 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Hello World!!");
 
 app.Run();
+
